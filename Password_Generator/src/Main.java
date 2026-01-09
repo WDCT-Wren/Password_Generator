d33aee85dd930d451a568b0fef5243e7103ade21
WDCT-Wren <wrenctulio@gmail.com>
2026-01-09 21:54:28 +0800

    Finished Password generating class (w/ checker)

--- Password_Generator/src/Main.java
+++ Password_Generator/src/Main.java
@@ -18,13 +18,8 @@ public class Main {
         System.out.print("Will your desired password include special characters?>> ");
         boolean hasSpecialChars = scanner.nextBoolean();
 
-        String password = "";
+        String password = passwordBuilder(digit, hasLowerCase, hasNumbers, hasSpecialChars, hasUpperCase);
 
-        boolean validPassword = false;
-        while (!validPassword) {
-            password = passwordBuilder(digit, hasLowerCase, hasNumbers, hasSpecialChars, hasUpperCase);
-            
-        }    
         System.out.println("Password generated: " + password);
         scanner.close();
     }
@@ -33,49 +28,58 @@ public class Main {
         String lowerCases = "abcdefghijklmnopqrstuvwxyz"; //String containing all possible lowercase characters
         String upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //String containing all possible uppercase characters
         String numericalChars = "0123456789"; //String containing all possible numerical values
-        String specialChars = "!@#$%^&*:';_-=+"; //String containing commonly used special characters
+        String specialChars = "!@#$%^&*:';_-"; //String containing commonly used special characters
         
         String password = "";
-        for (int i = 0; i < digit; i++) {
-            int rand = (int)(4 * Math.random());
+        boolean validPassword = false;
+        while (!validPassword) {
+            for (int i = 0; i < digit; i++) {
+                int rand = (int)(4 * Math.random());
 
-            switch (rand) {
-                case 0 -> { //number generation
-                    if (hasNumbers){
-                        rand = (int)(numericalChars.length()* Math.random());
-                        password += String.valueOf(numericalChars.charAt(rand));
+                switch (rand) {
+                    case 0 -> { //number generation
+                        if (hasNumbers){
+                            rand = (int)(numericalChars.length()* Math.random());
+                            password += String.valueOf(numericalChars.charAt(rand));
+                        }
+                        else {
+                            digit++;
+                        }
                     }
-                    else {
-                        digit++;
+                    case 1 -> { //lowercase character generation
+                        if (hasLowerCase) {
+                            rand = (int)(lowerCases.length()* Math.random());
+                            password += String.valueOf(lowerCases.charAt(rand));
+                        }
+                        else {
+                            digit++;
+                        }
+                    }
+                    case 2 -> { //uppercase character generation
+                        if (hasUpperCase) {
+                            rand = (int)(upperCases.length()* Math.random());
+                            password += String.valueOf(upperCases.charAt(rand));
+                        }
+                        else {
+                            digit++;
+                        }
+                    }
+                    case 3 -> { //special character generation
+                        if (hasSpecialChars) {
+                            rand = (int)(specialChars.length()* Math.random());
+                            password += String.valueOf(specialChars.charAt(rand));
+                        }
+                        else {
+                            digit++;
+                        }
                     }
                 }
-                case 1 -> { //lowercase character generation
-                    if (hasLowerCase) {
-                        rand = (int)(lowerCases.length()* Math.random());
-                        password += String.valueOf(lowerCases.charAt(rand));
-                    }
-                    else {
-                        digit++;
-                    }
-                }
-                case 2 -> { //uppercase character generation
-                    if (hasUpperCase) {
-                        rand = (int)(upperCases.length()* Math.random());
-                        password += String.valueOf(upperCases.charAt(rand));
-                    }
-                    else {
-                        digit++;
-                    }
-                }
-                case 3 -> { //special character generation
-                    if (hasSpecialChars) {
-                        rand = (int)(specialChars.length()* Math.random());
-                        password += String.valueOf(specialChars.charAt(rand));
-                    }
-                    else {
-                        digit++;
-                    }
-                }
+            }
+            if (passwordValidator(password, hasLowerCase, hasNumbers, hasSpecialChars, hasUpperCase)) {
+                return password;
+            }
+            else {
+                password = "";
             }
         }
         return password;
@@ -86,28 +90,21 @@ public class Main {
             boolean hasNumber = password.matches(".*[0-9].*"); //Regex check if password contains a number
             boolean hasLower = password.matches(".*[a-z].*"); //Regex check if password contains a lowercase letter
             boolean hasCapital = password.matches(".*[A-Z].*"); //Regex check if password contains a capital letter
-            boolean hasSpecialChar = password.matches(".*[!@#$%^&*:;',.].*"); //Regex check if password contains a special character
+            boolean hasSpecialChar = password.matches(".*[!@#$%^&*:';_-].*"); //Regex check if password contains a special character
 
-            if (password.length() < 8) {
-                return false;
+            if (hasLowerCase) {
+                if (!hasLower) {return false;}
             }
-            /* TODO: Fix this and make sure that it checks every part. The return in all else if might be wrong so recheck. 
-            else if (!hasLower && !hasLowerCase) {
-                return false;
+            if (hasNumbers) {
+                if (!hasNumber) {return false;}
             }
-            else if (!hasNumber && !hasNumbers) {
-                return false;
+            if (hasUpperCase) {
+                if (!hasCapital) {return false;}
             }
-            else if (!hasCapital && !hasUpperCase) {
-                return false;
+            if (hasSpecialChars) {
+                if (!hasSpecialChar) {return false;}
             }
-            else if (!hasSpecialChar && !hasSpecialChars) {
-                return false;
-            }
-            else {
-                return true;
-            }
-            */
+            return true;
         }
     } 
 }
